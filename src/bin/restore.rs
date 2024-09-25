cd0e6981d004e5c20590e288ea5ba3cfa4806fcf
chamila <chamila.sujeewa@gmail.com>
2024-09-25 22:15:51 +1200

    Add initial ratatui structure

--- src/bin/restore.rs
+++ src/bin/restore.rs
@@ -3,12 +3,25 @@ use std::error::Error;
 use std::fs::{read_dir, read_to_string};
 use std::path::PathBuf;
 
+use crossterm::event::{self, Event, KeyCode};
 use libtrash::*;
+use ratatui::backend::CrosstermBackend;
+use ratatui::crossterm::event::DisableMouseCapture;
+use ratatui::crossterm::event::EnableMouseCapture;
+use ratatui::crossterm::execute;
+use ratatui::crossterm::terminal::{disable_raw_mode, LeaveAlternateScreen};
+use ratatui::crossterm::terminal::{enable_raw_mode, EnterAlternateScreen};
+use ratatui::layout::{Constraint, Direction, Layout};
+use ratatui::style::{Color, Modifier, Style};
+use ratatui::text::Span;
+use ratatui::widgets::{Block, Borders, List, ListItem};
+use ratatui::Terminal;
+use std::fs;
+use std::io;
+use std::path::Path;
 use urlencoding::decode;
 
 const VERBOSE_MODE: bool = false;
-const CMD_LIST: &str = "list";
-const CMD_RESTORE: &str = "restore";
 
 struct TrashedFile {
     OriginalFile: PathBuf,
@@ -16,13 +29,8 @@ struct TrashedFile {
     File: PathBuf,
 }
 
-fn main() {
-    // list trash
-    // let args: Vec<String> = env::args().skip(1).collect();
-    // let command = args[0].clone();
-    // match command.as_str() {
-    //     // CMD_LIST => {
-    //
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut files: Vec<TrashedFile> = vec![];
     // get user trash directory
     let user_home = get_home_dir().expect("couldn't get user home directory");
     let user_trash_dir = TrashDirectory::resolve_for_file(&user_home, VERBOSE_MODE)
@@ -30,31 +38,129 @@ fn main() {
 
     // iterate through entries in files and read the matching trashinfo, show the filename based on the entry
     // in trashinfo
-    let home_files: Vec<TrashedFile> =
+    let mut home_files: Vec<TrashedFile> =
         get_trashed_files(user_trash_dir).expect("error while iterating trash files");
+    files.append(&mut home_files);
     //
     // todo: do the same for every mounted drive
 
-    for trashed_file in home_files {
-        println!(
-            "{} \t {} \t {}",
-            trashed_file
-                .OriginalFile
-                .file_name()
-                .expect("file-name")
-                .to_str()
-                .expect("file-name"),
-            trashed_file.DeletionDate,
-            trashed_file.OriginalFile.display(),
-        );
+    // Setup terminal
+    enable_raw_mode();
+    let mut stderr = io::stderr();
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    let mut selected_index = 0;
+    loop {
+        terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([Constraint::Percentage(100)].as_ref())
+                .split(f.area());
+
+            let list_items: Vec<ListItem> = files
+                .iter()
+                .enumerate()
+                .map(|(i, file)| {
+                    // for trashed_file in files {
+                    // println!(
+                    //     "{} \t {} \t {}",
+                    //     trashed_file
+                    //         .OriginalFile
+                    //         .file_name()
+                    //         .expect("file-name")
+                    //         .to_str()
+                    //         .expect("file-name"),
+                    //     trashed_file.DeletionDate,
+                    //     trashed_file.OriginalFile.display(),
+                    // );
+                    let original_path = file
+                        .OriginalFile
+                        .file_name()
+                        .expect("file_name")
+                        .to_os_string()
+                        .into_string()
+                        .unwrap();
+
+                    let entry = if i == selected_index {
+                        Span::styled(
+                            original_path,
+                            Style::default()
+                                .bg(Color::Yellow)
+                                .fg(Color::Black)
+                                .add_modifier(Modifier::BOLD),
+                        )
+                    } else {
+                        Span::raw(original_path)
+                    };
+                    // let entry = Span::raw(original_path);
+                    // let item = ListItem::new(entry);
+                    ListItem::new(entry)
+                    // list_items.push(item);
+                })
+                .collect();
+
+            let list = List::new(list_items)
+                .block(Block::default().borders(Borders::ALL).title("Trash"))
+                .highlight_style(Style::default().fg(Color::Yellow));
+
+            f.render_widget(list, chunks[0]);
+        })?;
+
+        // Handle input events
+        // if event::poll(std::time::Duration::from_millis(100))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Release {
+                // Skip events that are not KeyEventKind::Press
+                continue;
+            }
+
+            match key.code {
+                KeyCode::Up => {
+                    if selected_index > 0 {
+                        selected_index -= 1;
+                    }
+                }
+                KeyCode::Down => {
+                    if selected_index < files.len() - 1 {
+                        selected_index += 1;
+                    }
+                }
+                KeyCode::Enter => {
+                    let selected_file = &files[selected_index];
+                    println!(
+                        "Executing action on: {}",
+                        selected_file
+                            .OriginalFile
+                            .file_name()
+                            .unwrap()
+                            .to_str()
+                            .unwrap()
+                    );
+                    // Here you can define the action for the file (e.g., open the file, run a command, etc.)
+                }
+                KeyCode::Char('q') => {
+                    // Exit on 'q'
+                    break;
+                }
+                _ => {}
+            }
+        }
+        // }
     }
 
-    // }
-    // CMD_RESTORE => {
-    // if args.len() < 2 {
-    //     msg_err("missing filename");
-    //     std::process::exit(1); //todo
-    // }
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture,
+    )?;
+    terminal.show_cursor()?;
+
+    ratatui::restore();
+    Ok(())
 
     // let trashed_file_name = args[1];
     // figure out trash root for specified file
@@ -104,16 +210,6 @@ fn get_trashed_files(trash_dir: TrashDirectory) -> Result<Vec<TrashedFile>, Box<
             File: child_path,
         };
         files.push(trashed_entry);
-        // println!(
-        //     "{} \t {} \t {}",
-        //     original_file
-        //         .file_name()
-        //         .expect("file-name")
-        //         .to_str()
-        //         .expect("file-name"),
-        //     deletion_date,
-        //     original_path
-        // );
     }
 
     Ok(files)
